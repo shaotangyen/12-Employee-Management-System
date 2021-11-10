c3d3c832d5e716252d199313976590ea2fb99714
Shao <shaotang.yen@gmail.com>
2021-11-10 20:35:16 +1100

    initial set up

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE departments();
+
+CREATE TABLE roles();
+
+CREATE TABLE employees();
---
+++ server.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+// to do
+// View All Departments
+//  -> table showing IDs, Department
+// View All Roles
+//  -> table showing IDs, Title, Department, Salary
+// View All Employees
+//  -> table showing IDs, First Name, Lat Name, Title, Department, Salary, Manager
+// Add Department
+//  -> Enter name of department, adding department to the db
+// Add Role
+//  -> Enter name, salary, department, adding role to the db
+// Add Employee
+//  -> Enter first name, last name, role, manager, adding employee to the db
+// Update Employee Role
+//  -> promtp to select an employee from list of all employes
+//     to update their new role, updating this info to the db
+// Quit
